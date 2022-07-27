5c6b7c323b0fef2b6e8f92fa0ad7552af0cc862d
LLLSSSWWW <107101028+LLLSSSWWW@users.noreply.github.com>
2022-07-27 19:02:32 +0900

    add 10773.cpp
    stack

---
+++ 10773.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+	int N, Num, sum	= 0;
+	cin >> N;
+	stack<int> s;
+
+	for (int i = 0; i < N; i++) {
+		cin >> Num;
+		if (Num == 0) {
+			s.pop();
+		}
+		else {
+			s.push(Num);
+		}
+	}
+
+	while (!s.empty()) {
+		sum += s.top();
+		s.pop();
+	}
+
+	cout << sum << endl;
+
+}
