efa7afaca3831cdecffbcb37c3e2e69841dcdd29
LLLSSSWWW <107101028+LLLSSSWWW@users.noreply.github.com>
2022-07-27 15:00:33 +0900

    add 2798.cpp
    bruteForce

---
+++ 2798.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void blackJack(int N, int M) {
+	if (N < 3 || N > 100) {
+		cout << "카드 개수를 다시 맞추세요" << endl;
+		
+	}
+	else if (M < 10 || M > 300000) {
+		cout << "카드의 범위를 다시 맞추세요" << endl;
+		
+	}
+
+	int arr[100] = {0, };
+	int max = 0, current = 0;
+
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+	}
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			for (int k = j + 1; k < N; k++) {
+				current = arr[i] + arr[j] + arr[k];
+				if (current <= M && current > max)
+					max = current;
+			}
+		}
+	}
+
+	cout << max << endl;;
+}
+
+
+
+
+int main(void) {
+	int N, M;
+	cin >> N >> M;
+
+	blackJack(N, M);
+}
